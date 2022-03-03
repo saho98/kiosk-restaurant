d66982bae7597aee13b437c88a778ac4bd746ba5
saho98 <sahinb98@gmail.com>
2022-03-04 01:16:38 +0300

    update
    anlık sipariş durumunda masaların rengini değiştiren kodu yazdım.

--- fonks/conn.php
+++ fonks/conn.php
@@ -1,30 +1,60 @@
 <?php
 try{
+    //PDO ile bağlantı
     $conn = new PDO("mysql:host=localhost;dbname=db_restaurant","root","");
     $conn->query("set CHARSET UTF8");
+    //Bağlantı kurulamaması halinde alınacak mesaj
 }catch(PDOException $a){
     die("Bağlantı kurulamadı.".$a->getMessage());
 }
-class sistem{
-    
-    private function sorgu($con,$stmt){
 
+class sistem{
+    //tek fonksiyon ile gelecek sorguların parametreleri çekiliyor
+    private function sorgu($con,$stmt){
+        
         $b=$con->prepare($stmt);
         $b->execute();
         return $b;
-    }
     
+    }
+
+    
+    
+    //veritabanındaki masaları fetch_assoc komutu ile yazdırıyoruz
     public function catchmasa($conn){
+        
         $stm ="select * from tblmasalar";
         $b=$this->sorgu($conn,$stm);
         while($da = $b->fetch(PDO::FETCH_ASSOC)){
-            echo '<div class="col-md-2 col-sm-3 border ml-5  bg-danger text-center text-white" id="masalar">'.$da["tableName"].'</div>';
+
+            //pdo prepare parametre gerektirdiği için sorgu fonksiyonunu kullanamadım
+            $stm = $conn->prepare('select * from tblanlıksiparis where tableId =?');
+            $stm->execute(array($da["id"]));
+            
+            
+            //bu komut eğer anlık sipariş tablosunda veri var ise masa kutucuklarının yeşil olması,aksi durumda kırmızı olması için
+            $count = $stm->rowCount();
+            if($count==0){$renk = "danger";}else{$renk = "success";}
+            
+            //çekilecek her tablo için bu html kodları gelicek.    
+            echo '<div class="col-md-2 col-sm-3 border ml-5  bg-'.$renk.' text-center text-white" id="masalar">
+            <div class="mx-auto p-2 text-center text-white"  id="masa" ><a href="masadetay.php?tableId='.$da["id"].'">'.$da["tableName"].'</a></div></div>';
+
         }
 
     }
 
+    public function masarow($conn){
+        
+        $stm ="select * from tblmasalar";
+        $b=$this->sorgu($conn,$stm); 
+        echo count($b->fetchAll(PDO::FETCH_ASSOC));
+
+    }
+
 
 }
+
 
 
 
---
+++ masadetay.php
@@ -0,0 +1,51 @@
+<?php include 'fonks\conn.php'; 
+    $masa= new sistem();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restoran Sipariş Sistemi</title>
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+</head>
+<style>
+    
+</style>
+<body>
+<?php 
+@$masaid=$_GET["tableId"];
+
+if($masaid!=""){
+
+?>
+
+
+
+    <div class="container-fluid">
+        <div class="row border" style="min-height:700px">
+            <div class="col-md-2 border-right border-grey">
+                 <div class="row">
+                     <div class="col-md-12 border-bottom bg-info text-white" style="min-height: 100px;"></div>
+                     <div class="col-md-12 border-bottom bg-light ">s</div>
+                     <div class="col-md-12 border-bottom bg-light ">s</div>
+                     <div class="col-md-12 border-bottom bg-light ">s</div>
+                 </div>
+            </div>
+            <div class="col-md-10">
+
+            </div>
+        </div>
+
+    </div>
+<?php 
+}else{
+    echo "Hata";
+}
+?>
+<script src="assets/jquery-3.6.0.min.js"></script>
+</body>
+</html>
+
